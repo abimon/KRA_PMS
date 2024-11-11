0b21b2ecee90a2c9ee352e0b8ad5618a8187f74c
abimon <eddyabby59@gmail.com>
2024-11-11 19:19:46 +0300

    log payment

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('api/payment/callback/{account}', [MpesaController::class, 'update']);
+Route::post('/payment/callback/{account}', [MpesaController::class, 'update']);
