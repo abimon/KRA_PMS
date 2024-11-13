46cfae6c6d9ca60cadf6425ab6951684b823cead
abimon <eddyabby59@gmail.com>
2024-11-14 01:45:16 +0300

    payments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -133,6 +133,134 @@
 
 <div class="container-fluid pt-4 px-4">
     <div class="h-100 bg-secondary rounded p-4">
+        <div class="d-flex align-items-between justify-content-between mb-4">
+            <h6 class="mb-0">Salary Reports</h6>
+            <div class="text-center mb-2">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pay">
+                    Register PAYE
+                </button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#record">
+                    Get PAYE Records
+                </button>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade" id="pay" tabindex="-1" aria-labelledby="pay" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="payee">Register PAYE</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="{{route('payee.store')}}" method="post">
+                            @csrf
+                            <div class="modal-body">
+                                <input type="hidden" name="user_id" value="{{Auth()->user()->id}}">
+                                <div class="row mb-3">
+                                    <label for="basic_salary"
+                                        class="col-md-4 col-form-label text-md-end text-dark">{{ __('Basic Salary') }}</label>
+
+                                    <div class="col-md-8">
+                                        <input id="basic_salary" type="number"
+                                            class="form-control @error('basic_salary') is-invalid @enderror"
+                                            name="basic_salary" value="{{ old('basic_salary') }}" required
+                                            autocomplete="basic_salary" autofocus>
+
+                                        @error('basic_salary')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="allowances"
+                                        class="col-md-4 col-form-label text-md-end text-dark">{{ __('Allowances') }}</label>
+
+                                    <div class="col-md-8">
+                                        <input id="allowances" type="number"
+                                            class="form-control @error('allowances') is-invalid @enderror"
+                                            name="allowances" value="{{ old('allowances') }}" required
+                                            autocomplete="allowances" autofocus>
+
+                                        @error('allowances')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="insurance"
+                                        class="col-md-4 col-form-label text-md-end text-dark">{{ __('S.H.I.F') }}</label>
+
+                                    <div class="col-md-8">
+                                        <input id="insurance" type="number"
+                                            class="form-control @error('insurance') is-invalid @enderror"
+                                            name="insurance" value="{{ old('insurance') }}" required
+                                            autocomplete="insurance" autofocus>
+
+                                        @error('insurance')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <label for="pension"
+                                        class="col-md-4 col-form-label text-md-end text-dark">{{ __('Pension') }}</label>
+
+                                    <div class="col-md-8">
+                                        <input id="pension" type="number"
+                                            class="form-control @error('pension') is-invalid @enderror" name="pension"
+                                            value="{{ old('pension') }}" required autocomplete="pension" autofocus>
+
+                                        @error('pension')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Modal -->
+            <div class="modal fade" id="record" tabindex="-1" aria-labelledby="payee" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="payee">Get PAYE Records</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form action="{{route('payee.edit', 3)}}" method="post">
+                            @csrf
+                            @method('GET')
+                            <div class="modal-body">
+                                <div class="row">
+                                    <label for="from" class="col-md-4">From</label>
+                                    <input type="date" name="from" id="" class="form-control">
+                                </div>
+                                <div class="row">
+                                    <label for="to" class="col-md-4">To</label>
+                                    <input type="date" name="to" id="" class="form-control">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table class="table text-start align-middle table-bordered table-hover mb-0 table-responsive">
             <thead>
                 <tr class="text-white">
